cc61a499cd7f670cc3286270eda419a0eaa6b2b7
Szymx0504 <szymonjankovski480@gmail.com>
2025-12-23 23:46:13 +0100

    fixed socket.io connections in production

--- frontend/src/services/socket.jsx
+++ frontend/src/services/socket.jsx
@@ -1,28 +1,33 @@
-import {io} from 'socket.io-client';
+import { io } from "socket.io-client";
 
 class SocketService {
-    constructor(){
-        this.socket = null;
-        // this.isInGame = false;
-    }
+  constructor() {
+    this.socket = null;
+    // this.isInGame = false;
+  }
 
-    connect() {
-        if(!this.socket){
-            this.socket = io("http://localhost:8080", {
-                reconnection: true,
-                reconnectionAttempts: 5,
-                reconnectionDelay: 2000
-            });
+  connect() {
+    if (!this.socket) {
+      this.socket = io(
+        window.location.hostname === "localhost"
+          ? "http://localhost:8080"
+          : window.location.origin,
+        {
+          reconnection: true,
+          reconnectionAttempts: 5,
+          reconnectionDelay: 2000,
         }
-        return this.socket;
+      );
     }
+    return this.socket;
+  }
 
-    disconnect(){
-        if(this.socket){
-            this.socket.disconnect();
-            this.socket = null;
-        }
+  disconnect() {
+    if (this.socket) {
+      this.socket.disconnect();
+      this.socket = null;
     }
+  }
 }
 
 export default new SocketService();
